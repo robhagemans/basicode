747835d06d083b8c3531573e2e6ee7457c956488
Rob Hagemans <robhagemans@yahoo.co.uk>
2016-12-04 21:20:03 +0000

    add checksum checker script

---
+++ checksum.py
@@ -0,0 +1,11 @@
+import sys
+
+# include the start and end byte
+checksum = 0x83 ^ 0x82
+
+with open(sys.argv[1], 'rb') as f:
+    # basicode has CR line endings
+    for char in f.read().replace('\r\n', '\r').replace('\n', '\r'):
+        checksum ^= ord(char) ^ 0x80
+
+print '%02x' % checksum
