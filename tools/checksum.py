ce86da65cb4dd1ecfac8e3d8b5739ca23ac2d589
Rob Hagemans <robhagemans@yahoo.co.uk>
2017-04-17 19:01:53 +0100

    update checksum tool to enable checksums for multiple files

--- tools/checksum.py
+++ tools/checksum.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 import sys
 
-# include the start and end byte
-checksum = 0x83 ^ 0x82
+for name in sys.argv[1:]:
+    # include the start and end byte
+    checksum = 0x83 ^ 0x82
 
-with open(sys.argv[1], 'rb') as f:
-    # basicode has CR line endings
-    for char in f.read().replace('\r\n', '\r').replace('\n', '\r'):
-        checksum ^= ord(char) ^ 0x80
+    with open(name, 'rb') as f:
+        # basicode has CR line endings
+        for char in f.read().replace('\r\n', '\r').replace('\n', '\r'):
+            checksum ^= ord(char) ^ 0x80
 
-print '%02x' % checksum
+    print '%02x %s' % (checksum, name)
